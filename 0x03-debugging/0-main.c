2f3459734c913fecffd190c8c06ddd96558ac742
Albert Mpepo <albertmpepo@gmail.com>
2023-03-24 08:08:55 -0700

    fix update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
-void positive_or_negative(int i)
+/**
+* main - Test function for positive or negative
+* Return: 0
+*/
+int main(void)
 {
-	if (i < 0)
-	{
-		printf("%d is negative\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("%d is positive\n", i);
-	}
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
+
