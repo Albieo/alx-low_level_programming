957f431db0560026430e564e8a3af3d437cd6ad2
Albert Mpepo <albertmpepo@gmail.com>
2023-04-05 02:42:27 -0700

    problem 2 fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string.
+ *_strlen_recursion - returns the length of a string.
  *
  * @s: input pointer
  *
 */
 int _strlen_recursion(char *s)
 {
-    if (*s)
-    {
-        return (1 + _strlen_recursion(s + 1));
-    }
-    else
-    {
-        return (0);
-    }
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
 }
