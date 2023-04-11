cddf79dea67be90a6a33e71574929f8e98472c76
Albert Mpepo <albertmpepo@gmail.com>
2023-04-11 08:40:48 -0700

    tasks completed

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicate to new memory space location
+ *
+ * @str: char
+ *
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *word;
+	int i, j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	word = malloc(sizeof(char) * (i + 1));
+
+	if (word == NULL)
+		return (NULL);
+
+	for (r = 0; str[j]; j++)
+		word[j] = str[j];
+
+	return (word);
+}
