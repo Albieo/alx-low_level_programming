b0fa4e960adc11d1cd60b916e3b0c007ddfcd34f
Albert Mpepo <albertmpepo@gmail.com>
2023-04-17 18:35:37 -0700

    tasks completed

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - releases memory allocated for the struct dog
+ *
+ * @d: struct pointer to dog
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+		free(d -> name);
+		free(d -> owner);
+		free(d);
+}
