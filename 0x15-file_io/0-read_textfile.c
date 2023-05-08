c145b9e2b947e34be750cc9dd1054f6d2286650c
Albieo <albertmpepo@gmail.com>
2023-05-08 02:44:44 -0700

    some tasks completed

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ *
+ * @filename: file to read
+ * @letters: number of letter in file
+ *
+ * Return: the number of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t file_d;
+	ssize_t wrt;
+	ssize_t p;
+
+	file_d = open(filename, O_RDONLY);
+	if (file_d == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters)
+	p = read(file_d, buffer, letters);
+	wrt = write(STDOUT_FILENO, buffer, p);
+	free(buffer);
+	close(file_d);
+
+	return (wrt);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ *
+ * @filename: name of the file.
+ * @text_content: text to add to file.
+ *
+ * Return: 1 (Success), -1 (Error)
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_d, wrt, len = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		for (len = 0; text_content[len]; len++)
+			len++;
+	}
+
+	file_d = open(filename, O_WRONLY | O_APPEND);
+	wrt = write(file_d, text_content, len);
+
+	if (file_d == -1 || wrt == -1)
+	{
+		return (-1);
+	}
+	close(file_d);
+
+	return (1);
+}
