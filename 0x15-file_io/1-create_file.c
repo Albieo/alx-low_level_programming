ff8a621ab643d1d61a6657e8fee0f4eb689eb8cf
Albieo <albertmpepo@gmail.com>
2023-05-09 00:19:34 -0700

    task 1 fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content != NULL)
+	if (text_content)
 	{
-		for (len = 0; text_content[len]; len++)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
