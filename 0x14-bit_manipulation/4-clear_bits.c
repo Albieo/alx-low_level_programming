1e13f668c334a660cb81f091496fcd7ef75bbe1a
Albert Mpepo <albertmpepo@gmail.com>
2023-07-06 12:40:42 +0200

    changes made on 4

--- 0x14-bit_manipulation/4-clear_bits.c
+++ 0x14-bit_manipulation/4-clear_bits.c
@@ -8,14 +8,14 @@
  *
  * Return: 1 (Sucessful), -1 (Error)
  */
-int clear_bit(unsigned long int *n, unsigned int idx)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (idx >= 62)
+	if (index >= 62)
 	{
 		return (-1);
 	}
 
-	*n = (~(1UL << idx) & *n);
+	*n = (~(1UL << index) & *n);
 
 	return (1);
 }
