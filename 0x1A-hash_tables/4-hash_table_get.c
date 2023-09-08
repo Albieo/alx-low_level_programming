44f49ad5af84e55da71399108775133262a5735c
Albert Mpepo <albertmpepo@gmail.com>
2023-09-08 19:09:41 +0200

    4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *temp = ht->array[index];
 
-	if (temp != NULL)
+	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
 			return (temp->value);
