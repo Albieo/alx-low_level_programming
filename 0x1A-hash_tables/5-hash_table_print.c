2c7f5cb129183da76170e81ffd709edc6e2dcbc6
Albert Mpepo <albertmpepo@gmail.com>
2023-09-09 01:04:55 +0200

    5. >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - a function that prints a hash table.
+ * @ht: the hash table.
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp;
+	unsigned long int i, sep = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while(temp != NULL)
+		{
+			if (sep != 0)
+				printf(", ");
+
+			printf("%s: %s", temp->key, temp->value);
+			temp = temp->next;
+
+			if (sep == 0)
+				sep = 1;
+		}
+	}
+	printf("}\n");
+}
