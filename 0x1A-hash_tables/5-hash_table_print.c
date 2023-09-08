0eb87e29a0939d53146e1630dde37fccf279b8d6
Albert Mpepo <albertmpepo@gmail.com>
2023-09-09 01:48:47 +0200

    betty fix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -18,7 +18,7 @@ void hash_table_print(const hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		temp = ht->array[i];
-		while(temp != NULL)
+		while (temp != NULL)
 		{
 			if (sep != 0)
 				printf(", ");
