77784cb9fb931d5faa39772fa0127bbf3d698a02
Albert Mpepo <albertmpepo@gmail.com>
2023-09-08 18:40:41 +0200

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -44,7 +44,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		temp = temp->next;
 	}
 
-	if (ht->array[index] == NULL)
+	if (temp == NULL)
 		ht->array[index] = new_node;
 	else
 	{
