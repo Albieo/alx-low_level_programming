e9de3d43811f9a2b27408481f3908acc5f08be81
Albert Mpepo <albertmpepo@gmail.com>
2023-12-14 00:04:33 +0200

    Binary fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -5,28 +5,50 @@
  * @array: a pointer to the first element of the array to search in.
  * @size: the number of elements in array.
  * @value: the value to search for.
- * 
+ *
  * Return: the index of the value or -1 if array is NULL and value not found.
 */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0;
-	size_t right = (size - 1);
-	size_t mid;
+    size_t left = 0;
+    size_t right = size - 1;
+    size_t mid, i;
 
-	if (array == NULL)
-		return (-1);
+    if (array == NULL)
+	return -1;
 
-	while (left <= right)
-	{
-		mid = (left + right)/2;
-		if (array[mid] < value)
-			left = mid + 1;
-		else if (array[mid] > value)
-			right = mid - 1;
-		else
-			return (mid);
+    printf("Searching in array: ");
+    for (i = 0; i < size; i++)
+    {
+	printf("%i", array[i]);
+	if (i != size - 1)
+	    printf(", ");
+    }
+    printf("\n");
+
+    while (left <= right)
+    {
+	mid = (left + right) / 2;
+
+	if (array[mid] < value)
+	    left = mid + 1;
+	else if (array[mid] > value)
+	    right = mid - 1;
+	else
+	    return mid;
+
+	if (left <= right)
+		{
+	    printf("Searching in array: ");
+	    for (i = left; i <= right; ++i)
+	    {
+		printf("%i", array[i]);
+		if (i != right)
+		    printf(", ");
+	    }
+	    printf("\n");
 	}
+    }
 
-	return (-1);
+    return -1;
 }
